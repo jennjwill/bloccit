12b45bec1e55f201a29f8d0e5934f729a181fbc6
Jenn J Will <jennijwill@gmail.com>
2019-08-05 14:14:53 -0700

    checkpoint deploying to production completed

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 const app = require("./app"); //import app
 const http = require("http"); //import http module
-const server = http.createServer(app); //create server
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 
-server.listen(3000);
+const server = http.createServer(app);
+
+server.listen(port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNan(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 server.on("listening", () => {
-  console.log("server is listening for requests on port 3000");
+  console.log(
+    `server is listening for requests on port ${server.address().port}`
+  );
 });
