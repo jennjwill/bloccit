c56abf5ea1457388cab0a79d6c15b6093f4aaba9
Jenn J Will <jennijwill@gmail.com>
2019-08-05 13:56:13 -0700

    updated code up to heroku install

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express(); //init app
 
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app; //export app so we can pass it to Node server in server.js
