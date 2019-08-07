905fdfaf530cbf80fc224b3ecf0cf0aec8fd4eac
Jenn J Will <jennijwill@gmail.com>
2019-08-06 17:17:04 -0700

    finished with half of checkpoint code

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 appConfig.init();
-routeConfig.init(app);
+routeConfig.init(app, express);
 
 module.exports = app; //export app so we can pass it to Node server in server.js
