c9916b88ce3c86d04a46787927d2cd4b433e0878
Jenn J Will <jennijwill@gmail.com>
2019-08-09 10:58:00 -0700

    checkpoint topic resource: ERROR relation topic does not exist

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,10 +1,18 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING
-  }, {});
+  var Topic = sequelize.define(
+    "Topic",
+    {
+      title: DataTypes.STRING
+    },
+    {}
+  );
   Topic.associate = function(models) {
     // associations can be defined here
+    Topic.hasMany(models.Banner, {
+      foreignKey: "topicId",
+      as: "banners"
+    });
   };
   return Topic;
 };
