b2bcf6be5a2742a6cbf97133666065f8edfa1593
Jenn J Will <jennijwill@gmail.com>
2019-08-13 17:23:09 -0700

    code for advertisement CRUD written thru index view but not passing spec test for view

---
+++ src/controllers/advertisementController.js
@@ -0,0 +1,13 @@
+const adQueries = require("../db/queries.advertisements.js");
+
+module.exports = {
+  index(req, res, next) {
+    adQueries.getAllAds((err, advertisements) => {
+      if (err) {
+        res.redirect(500, "static/index");
+      } else {
+        res.render("advertisements/index", { advertisements });
+      }
+    });
+  }
+};
