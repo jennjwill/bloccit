0d266e6e8cf1ff60f970ac94d43903e22772ee1f
Jenn J Will <jennijwill@gmail.com>
2019-09-06 10:22:17 -0700

    checkpoint: comments completed

---
+++ src/policies/comment.js
@@ -0,0 +1,5 @@
+const ApplicationPolicy = require("./application");
+
+module.exports = class CommentPolicy extends ApplicationPolicy {
+  //the create and destroy methods of the parent class meet our needs, so we don't add code here
+};
