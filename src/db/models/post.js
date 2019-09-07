9c9d9dec2130101754c30316e86aec6cdb67e569
Jenn J Will <jennijwill@gmail.com>
2019-09-07 12:28:15 -0700

    coded voting unit tests and modeled voting assocs, migrated

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -42,6 +42,11 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "comments"
     });
+
+    Post.hasMany(models.Vote, {
+      foreignKey: "postId",
+      as: "votes"
+    });
   };
   return Post;
 };
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -34,6 +34,11 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       as: "comments"
     });
+
+    User.hasMany(models.Vote, {
+      foreignKey: "userId",
+      as: "votes"
+    });
   };
 
   User.prototype.isAdmin = function() {
